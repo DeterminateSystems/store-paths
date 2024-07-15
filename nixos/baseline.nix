65bbaeb25e83c7423a1c4cae067b38b02702e15e
Luc Perkins <lucperkins@gmail.com>
2024-07-15 16:48:31 -0700

    Add fileSystems config

--- nixos/baseline.nix
+++ nixos/baseline.nix
@@ -6,7 +6,20 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/nixos";
+    fsType = "ext4";
+  };
+
   networking.hostName = "minimal";
   networking.useDHCP = false;
   networking.interfaces.eth0.useDHCP = true;
+
+  services.openssh.enable = true;
+
+  users.users.justme = {
+    isNormalUser = true;
+    home = "/home/justme";
+    extraGroups = [ "wheel" ]; # for sudo access
+  };
 }
